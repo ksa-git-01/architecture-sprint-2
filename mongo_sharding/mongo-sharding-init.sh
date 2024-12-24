45842e78bf6e9b50a57b8af9aee014a0c2d5b6ee
serg <krutikov.s@gmail.com>
2024-12-24 23:01:07 +0300

    Task 2

---
+++ mongo_sharding/mongo-sharding-init.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+###
+# Инициализируем бд
+###
+
+docker exec -i configSrv mongosh --host configSrv --port 27017 <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+exit();
+EOF 
+
+docker exec -i shard1 mongosh --host shard1 --port 27018 <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" }
+      ]
+    }
+);
+exit();
+EOF
+
+docker exec -i shard2 mongosh --host shard2 --port 27019 <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 1, host : "shard2:27019" }
+      ]
+    }
+  );
+exit();
+EOF
+
+docker exec -i mongos_router mongosh --host mongos_router --port 27020 <<EOF
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27019");
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+use somedb
+
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+
+db.helloDoc.countDocuments() 
+exit(); 
+EOF
